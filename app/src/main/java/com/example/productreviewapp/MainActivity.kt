6ea51f852bb85a6ec183e8752d166c9e75547152
clc79100 <clc79100@lasallebajio.edu.mx>
2025-12-06 20:36:00 -0600

    Correccion en validacion de rutas para mostrar el NavBar

--- app/src/main/java/com/example/productreviewapp/MainActivity.kt
+++ app/src/main/java/com/example/productreviewapp/MainActivity.kt
@@ -8,10 +8,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.productreviewapp.ui.components.CustomNavigationBar
 import com.example.productreviewapp.ui.screens.AccountScreen
@@ -29,10 +31,20 @@ class MainActivity : ComponentActivity() {
         setContent {
             ProductReviewAppTheme {
                 val navController = rememberNavController()
+                val navBackStackEntry by navController.currentBackStackEntryAsState()
+                val currentDestination = navBackStackEntry?.destination?.route
+                val bottomBarDestinations = listOf(
+                    HomeScreenRoute::class.qualifiedName,
+                    VersusScreenRoute::class.qualifiedName,
+                    AccountScreenRoute::class.qualifiedName
+                )
                 Scaffold(
                     modifier = Modifier.fillMaxSize(),
                     bottomBar = {
-                            CustomNavigationBar(navController = navController)
+                        if (currentDestination in bottomBarDestinations) {
+                            CustomNavigationBar(navController)
+                        }
+
                     }
                 ){ innerPadding ->
                     NavHost(
